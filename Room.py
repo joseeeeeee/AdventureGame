9a88f64d8d3060e2b3f4a3578969f5a5b4d7e7da
josejaci18 <josejaci18@e3civichigh.com>
2018-02-28 14:38:27 -0800

    Initial commit

---
+++ Room.py
@@ -0,0 +1,60 @@
+class Room():
+    def __init__(self, name, description, id):
+        self.name = name
+        self.description = description
+        self.id = id
+        self.items = []
+        self.connectors = []
+        self.rooms = {}
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def add_room(self, direction, room):
+        self.rooms[direction] = room
+
+    def ass_connection(self, room, connector, actions):
+        for direction in actions:
+            self.rooms[direction] = room
+            self.connectors.append((connector, actions[0]))
+
+    def connect_rooms(self, direction, room):
+        opposite_direction = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+        self.add_room(direction, room)
+        room.add_room(opposite_direction[direction], self)
+
+    def enter_room(self):
+        print self.name
+        print
+        print self.description
+        print
+        if len(self.connectors) > 0:
+            for connector in self.connectors:
+                print "There is a " + connector[0]+ \
+                  " that goes " + connector[1]
+
+    def get_name(self):
+        return self.name
+
+    def is_valid_direction(self, direction):
+        return direction in self.rooms.keys()
+
+    def next_room(self, direction):
+        return self.rooms[direction]
+
+    def process_command(self, command, inventory):
+        if command in self.rooms.keys():
+            new_room = self.next_room(command)
+            return new_room
+        elif "get" in command:
+            for item in self.items:
+                if item.name in command:
+                    inventory.add(item)
+                    self.items.remove(item)
+                    return "You picked up the " + item.name + "."
+                continue
+            else:
+                    return "I don't know what you want to pick up."
+        else:
+            return None
+
